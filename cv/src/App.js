5be064ac9ca6238812d0144725e6e75da318cac8
Marius Bølset Gisleberg <mariubgi@stud.ntnu.no>
2022-09-07 22:44:50 +0200

    Første kodedag

--- cv/src/App.js
+++ cv/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="menu">
+        <ul className="list_elements">
+          <li className="list_menu">Home</li>
+          <li>About</li>
+        </ul>
+      </div>
+      <div className="overskrift">
+        <h1>Dette er min første React side</h1>
+      </div>
     </div>
   );
 }
