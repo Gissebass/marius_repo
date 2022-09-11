c716d5efb8b64657d78b907d6a6afbdd5d47a5b7
Marius Bølset Gisleberg <mariubgi@stud.ntnu.no>
2022-09-11 17:49:12 +0200

    lagt til plante og laget plass til burgermeny

--- cv/src/components/Navbar.js
+++ cv/src/components/Navbar.js
@@ -12,6 +12,10 @@ const Navbar = () => {
         </a>
         <a className="navbar_elements" href="#">Prosjekter</a>
         <a className="navbar_elements" href="#">Kontakt</a>
+        <div className="links" style={{border:"none"}}>
+        <img className="navbar_plante" src="/plante.png" alt="plante"></img>
+        </div>
+        <a className="navbar_elements" href="#">Burgermeny</a>
       </div>
     </nav>
   );
