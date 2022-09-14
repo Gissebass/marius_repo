65d2e5dc0c87744fe0c1aaa314a80125694f7250
Marius Bølset Gisleberg <mariubgi@stud.ntnu.no>
2022-09-14 21:44:42 +0200

    oppdatert nettsiden og laget struktur for dom og state

--- cv/src/App.js
+++ cv/src/App.js
@@ -1,15 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar.js";
 import "./components/Navbar.css";
-import MainComponent from "./components/MainComponent.js";
-import "./components/MainComponent.css";
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
     <div className="content">
-        <MainComponent />
     </div>
     </div>
   );
