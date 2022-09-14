82b56f26835713624937b47e09dffe3588926dd6
Marius Bølset Gisleberg <mariubgi@stud.ntnu.no>
2022-09-14 20:20:43 +0200

    Lagt til fonter og fjernet plantebilde

--- cv/src/components/Navbar.js
+++ cv/src/components/Navbar.js
@@ -10,12 +10,18 @@ const Navbar = () => {
         <a className="navbar_elements" href="#">
           About
         </a>
-        <a className="navbar_elements" href="#">Prosjekter</a>
-        <a className="navbar_elements" href="#">Kontakt</a>
-        <div className="links" style={{border:"none"}}>
-        <img className="navbar_plante" src="/plante.png" alt="plante"></img>
+        <a className="navbar_elements" href="#">
+          Prosjekter
+        </a>
+        <a className="navbar_elements" href="#">
+          Kontakt
+        </a>
+        <div className="links" style={{ border: "none" }}>
+          <img className="navbar_plante" src="#" alt="plante"></img>
         </div>
-        <a className="navbar_elements" href="#">Burgermeny</a>
+        <a className="navbar_elements" href="#">
+          Burgermeny
+        </a>
       </div>
     </nav>
   );
